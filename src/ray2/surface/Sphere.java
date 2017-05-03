8e2151193e5f57b28c776665474863d58c91695b
yt429 <yt429@cornell.edu>
2017-05-03 18:57:05 -0400

    implemented computeBoundingBox box and sphere

--- src/ray2/surface/Sphere.java
+++ src/ray2/surface/Sphere.java
@@ -109,7 +109,22 @@ public class Sphere extends Surface {
 	public void computeBoundingBox() {
 		// TODO#A7: Compute the bounding box and store the result in
 		// averagePosition, minBound, and maxBound.
-
+		Vector3d rVec = new Vector3d(this.radius, this.radius, this.radius);
+		Vector3d minPt = this.center.clone().sub(rVec);
+		Vector3d maxPt = this.center.clone().add(rVec);
+		Vector3d minPtWorld = this.tMat.clone().mulPos(minPt);
+		Vector3d maxPtWorld = this.tMat.clone().mulPos(maxPt);
+		this.minBound = new Vector3d(
+				Math.min(minPtWorld.x, maxPtWorld.x), 
+				Math.min(minPtWorld.y, maxPtWorld.y), 
+				Math.min(minPtWorld.z, maxPtWorld.z)
+			);
+		this.maxBound = new Vector3d(
+				Math.min(minPtWorld.x, maxPtWorld.x), 
+				Math.min(minPtWorld.y, maxPtWorld.y), 
+				Math.min(minPtWorld.z, maxPtWorld.z)
+			);
+		this.averagePosition = this.minBound.clone().add(this.maxBound).mul(1/2d);
 	}
 
 	/**

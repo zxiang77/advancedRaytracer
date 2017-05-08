4a90f89bd7cc0ff0ed4002b56377dbef01780fe8
Lydia <lydia.yuyantang@gmail.com>
2017-05-08 10:49:26 -0400

    debugged glaze

--- src/ray2/shader/Glazed.java
+++ src/ray2/shader/Glazed.java
@@ -55,45 +55,24 @@ public class Glazed extends Shader {
 		//  1) Compute the Fresnel term R
         //  2) Shade the substrate and multiply the result color by 1 - R
         //  3) Compute the reflected ray and call RayTracer.shadeRay on it, multiply result color by R
+		outIntensity.setZero();
 		
-		Vector3d Norig = record.normal.clone();
 		Vector3d V = ray.direction.clone().negate();
-
+		Vector3d N = record.normal.clone();
+		
+		double R = fresnel(N, V, this.refractiveIndex);
+		
 		Colord reflectedIntensity = new Colord();
 		Colord substrateIntensity = new Colord();
-		Vector3d N;
-		double r1, r2;
+		substrate.shade(substrateIntensity, scene, ray, record, depth+1);
 
-		double cos = getCos(Norig, V);
-		double R = fresnel(Norig, V, this.refractiveIndex);
-		if (cos < 0) {
-			// ray comes from inside of the surface
-			r1 = this.refractiveIndex;
-			r2 = 1d;
-			N = Norig.clone().negate();
-		} else {
-			// ray comes from outside
-			r1 = 1d;
-			r2 = this.refractiveIndex;
-			N = Norig;
-		}
-		
-		double c1 = getCos(N, V);
-		double s1 = cosToSin(c1);
 		Vector3d reflected = getReflected(V, N);
 		Ray reflectedRay = new Ray(record.location, reflected.normalize());
 		reflectedRay.makeOffsetRay();
 		RayTracer.shadeRay(reflectedIntensity, scene, reflectedRay, depth + 1);
-		if (s1 * r1 >= r2) {
-			// internal reflection occurs
-			outIntensity.set(reflectedIntensity);
-		} else {
-			double s2 = s1 * r1 / r2;
-			Vector3d refracted = getRefracted(V, N, s2);
-			Ray substrateRay = new Ray(record.location, refracted.normalize());
-			substrateRay.makeOffsetRay();
-			substrate.shade(substrateIntensity, scene, substrateRay, record, depth + 1);
-			outIntensity.set(reflectedIntensity.mul(R).add(substrateIntensity.mul(1d-R)));
-		}
+		
+		outIntensity.set(substrateIntensity.mul(1-R).add(reflectedIntensity.mul(R)));
+
 	}
+	
 }
